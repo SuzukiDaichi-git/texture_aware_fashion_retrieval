95aae00541082d165a3a2389addfb132c55798ad
Daichi Suzuki <suzuki@hal.t.u-tokyo.ac.jp>
2023-04-16 18:44:36 +0900

    add FashionViL code

---
+++ FashionVIL/mmf_baseline/models/fashionvil/contrastive.py
@@ -0,0 +1,131 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from typing import Dict
+
+import torch
+from mmf.models.composition import NormalizationLayer
+from mmf.models.fashionvil.base import FashionViLBaseModel
+from mmf.models.fashionvil.combiner import Combiner
+from torch import Tensor
+
+
+# class FashionViLForContrastive(FashionViLBaseModel):
+#     def __init__(self, config):
+#         super().__init__(config)
+#         self.norm_layer = NormalizationLayer()
+#         feature_dim = 768
+#         projection_dim = 2560
+#         hidden_dim = 5120
+#         self.img_combiner = Combiner(feature_dim, projection_dim, hidden_dim)
+#         self.txt_combiner = Combiner(feature_dim, projection_dim, hidden_dim)
+
+#     def flatten_for_bert(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
+#         to_be_flattened = ["input_ids_g", "segment_ids_g", "input_ids_l", "segment_ids_l"]
+#         to_be_flattened_dim = ["image_g", "image_l"]
+#         flattened = self.flatten(sample_list, to_be_flattened, to_be_flattened_dim)
+#         return flattened
+
+#     def add_post_flatten_params(
+#         self, sample_list: Dict[str, Tensor]
+#     ) -> Dict[str, Tensor]:
+#         b, l, _ = sample_list["image_g"].shape
+#         device = sample_list["image_g"].device
+#         sample_list["visual_embeddings_type"] = torch.zeros(
+#             (b, l), device=device
+#         ).long()
+#         return sample_list
+
+#     def _forward(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
+#         visual_embeddings_g, _, _ = self.bert.get_image_embedding(
+#             sample_list["image_g"],
+#             sample_list["visual_embeddings_type"],
+#         )
+#         visual_embeddings_l, _, _ = self.bert.get_image_embedding(
+#             sample_list["image_l"],
+#             sample_list["visual_embeddings_type"],
+#         )
+#         visual_embeddings = self.img_combiner.combine_features(visual_embeddings_g, visual_embeddings_l)
+#         visual_embeddings = visual_embeddings.mean(dim=1)
+#         visual_embeddings = self.norm_layer(visual_embeddings)
+
+#         text_embeddings_g, _, _ = self.bert.get_text_embedding(
+#             sample_list["input_ids_g"],
+#             sample_list["segment_ids_g"],
+#             sample_list["input_mask_g"],
+#         )
+#         masks_g = sample_list["input_mask_g"]
+#         text_embeddings_g = text_embeddings_g * masks_g.unsqueeze(2)
+#         text_embeddings_g = torch.sum(text_embeddings_g, dim=1) / (
+#             torch.sum(masks_g, dim=1, keepdim=True)
+#         )
+#         text_embeddings_l, _, _ = self.bert.get_text_embedding(
+#             sample_list["input_ids_l"],
+#             sample_list["segment_ids_l"],
+#             sample_list["input_mask_l"],
+#         )
+#         masks_l = sample_list["input_mask_l"]
+#         text_embeddings_l = text_embeddings_l * masks_l.unsqueeze(2)
+#         text_embeddings_l = torch.sum(text_embeddings_l, dim=1) / (
+#             torch.sum(masks_l, dim=1, keepdim=True)
+#         )
+#         text_embeddings = self.txt_combiner.combine_features(text_embeddings_g, text_embeddings_l)
+#         # text_embeddings = text_embeddings[:, 0]
+        
+#         text_embeddings = self.norm_layer(text_embeddings)
+
+#         output_dict = {
+#             "scores": visual_embeddings,
+#             "targets": text_embeddings,
+#         }
+#         return output_dict
+
+
+
+
+class FashionViLForContrastive(FashionViLBaseModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.norm_layer = NormalizationLayer()
+
+    def flatten_for_bert(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        to_be_flattened = ["input_ids", "segment_ids"]
+        to_be_flattened_dim = ["image"]
+        flattened = self.flatten(sample_list, to_be_flattened, to_be_flattened_dim)
+        return flattened
+
+    def add_post_flatten_params(
+        self, sample_list: Dict[str, Tensor]
+    ) -> Dict[str, Tensor]:
+        b, l, _ = sample_list["image"].shape
+        device = sample_list["image"].device
+        sample_list["visual_embeddings_type"] = torch.zeros(
+            (b, l), device=device
+        ).long()
+        return sample_list
+
+    def _forward(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        visual_embeddings, _, _ = self.bert.get_image_embedding(
+            sample_list["image"],
+            sample_list["visual_embeddings_type"],
+        )
+        visual_embeddings = visual_embeddings.mean(dim=1)
+        visual_embeddings = self.norm_layer(visual_embeddings)
+
+        text_embeddings, _, _ = self.bert.get_text_embedding(
+            sample_list["input_ids"],
+            sample_list["segment_ids"],
+            sample_list["input_mask"],
+        )
+        # text_embeddings = text_embeddings[:, 0]
+        masks = sample_list["input_mask"]
+        text_embeddings = text_embeddings * masks.unsqueeze(2)
+        text_embeddings = torch.sum(text_embeddings, dim=1) / (
+            torch.sum(masks, dim=1, keepdim=True)
+        )
+        text_embeddings = self.norm_layer(text_embeddings)
+
+        output_dict = {
+            "scores": visual_embeddings,
+            "targets": text_embeddings,
+        }
+        return output_dict
---
+++ FashionVIL/mmf_baseline/models/fashionvil/fashionvil.py
@@ -0,0 +1,264 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from typing import Dict
+
+import torch
+from mmf.common.registry import registry
+from mmf.models import BaseModel
+from mmf.models.fashionvil.classification import FashionViLForClassification
+from mmf.models.fashionvil.composition import FashionViLForComposition
+from mmf.models.fashionvil.contrastive import FashionViLForContrastive
+from mmf.models.fashionvil.pretraining import FashionViLForPretraining
+from mmf.models.fashionvil.combiner import Combiner
+
+from mmf.utils.build import build_image_encoder
+from mmf.utils.distributed import broadcast_tensor
+from mmf.utils.general import filter_grads
+from mmf.utils.modeling import get_fashionvil_configured_parameters
+from numpy.random import choice
+from torch import Tensor
+
+
+# @registry.register_model("fashionvil")
+# class FashionViL(BaseModel):
+#     def __init__(self, config):
+#         super().__init__(config)
+#         self.config = config
+#         self.training_head_type = config.training_head_type
+
+#         self.double_view = config.get("double_view", False)
+#         self.freeze_image_encoder = config.get("freeze_image_encoder", False)
+
+#         if self.training_head_type == "pretraining":
+#             self.task_for_inference = config.task_for_inference
+#             self.tasks = config.tasks
+#             self.tasks_sample_ratio = config.get("tasks_sample_ratio", None)
+
+#     @classmethod
+#     def config_path(cls):
+#         return "configs/models/fashionvil/defaults.yaml"
+
+#     def train(self, mode=True):
+#         super().train(mode)
+#         if self.freeze_image_encoder:
+#             self.image_encoder.eval()
+
+#     def build(self):
+#         self.image_encoder_g = build_image_encoder(
+#             self.config.image_encoder, self.config.direct_features_input
+#         )
+#         self.image_encoder_l = build_image_encoder(
+#             self.config.image_encoder, self.config.direct_features_input
+#         )
+#         if self.freeze_image_encoder:
+#             self.image_encoder = self.image_encoder.eval()
+#             for param in self.image_encoder.parameters():
+#                 param.requires_grad = False
+#         if self.training_head_type == "pretraining":
+#             self.model = FashionViLForPretraining(self.config)
+#         elif self.training_head_type == "classification":
+#             self.model = FashionViLForClassification(self.config)
+#         elif self.training_head_type == "composition":
+#             self.model = FashionViLForComposition(self.config)
+#         elif self.training_head_type == "contrastive":
+#             self.model = FashionViLForContrastive(self.config)
+#         else:
+#             raise NotImplementedError
+
+#         if self.config.special_visual_initialize:
+#             self.model.bert.embeddings.initialize_visual_from_pretrained()
+
+#         if getattr(self.config, "freeze_base", False):
+#             for p in self.model.bert.parameters():
+#                 p.requires_grad = False
+
+#     def get_optimizer_parameters(self, config):
+#         base_lr = config.optimizer.params.lr
+#         weight_decay = config.optimizer.params.weight_decay
+#         lr_multiplier = self.config.lr_multiplier
+
+#         image_encoder_g_params = [
+#             {
+#                 "params": filter_grads(self.image_encoder_g.parameters()),
+#                 "lr": base_lr * lr_multiplier,
+#             }
+#         ]
+#         image_encoder_l_params = [
+#             {
+#                 "params": filter_grads(self.image_encoder_l.parameters()),
+#                 "lr": base_lr * lr_multiplier,
+#             }
+#         ]
+#         img_combiner_params = [
+#             {
+#                 "params": filter_grads(self.model.img_combiner.parameters()),
+#                 "lr": base_lr * lr_multiplier * 10,
+#             }
+#         ]
+#         txt_combiner_params = [
+#             {
+#                 "params": filter_grads(self.model.txt_combiner.parameters()),
+#                 "lr": base_lr * lr_multiplier * 10,
+#             }
+#         ]
+
+#         lr_filter = []
+#         lr_filter.append("bert.embeddings.projection.weight")
+#         lr_filter.append("bert.embeddings.projection.bias")
+#         if self.training_head_type == "classification":
+#             lr_filter.append("classifier")
+#         elif self.training_head_type == "pretraining":
+#             lr_filter.append("heads")
+
+#         # print("self.model = ", self.model)
+#         # print("self.model.bert = ", self.model.bert)
+#         # bert_params = get_fashionvil_configured_parameters(
+#         #     self.model.bert,
+#         #     base_lr,
+#         #     weight_decay,
+#         #     lr_filter,
+#         #     lr_multiplier,
+#         # )
+#         # norm_params = [
+#         #     {
+#         #         "params": filter_grads(self.model.norm_layer.parameters()),
+#         #         "lr": base_lr * lr_multiplier * 10,
+#         #     }
+#         # ]
+#         # return image_encoder_g_params + image_encoder_l_params + img_combiner_params + txt_combiner_params + bert_params + norm_params
+#         return image_encoder_g_params + image_encoder_l_params + img_combiner_params + txt_combiner_params
+
+#     def forward(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
+
+#         sample_list.original_image_g = sample_list.image_g
+#         sample_list.image_g = self.image_encoder_g(sample_list.image_g)
+#         sample_list.original_image_l = sample_list.image_l
+#         sample_list.image_l = self.image_encoder_l(sample_list.image_l)
+
+#         return self.model(sample_list)
+
+
+
+
+@registry.register_model("fashionvil")
+class FashionViL(BaseModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.training_head_type = config.training_head_type
+
+        self.double_view = config.get("double_view", False)
+        self.freeze_image_encoder = config.get("freeze_image_encoder", False)
+
+        if self.training_head_type == "pretraining":
+            self.task_for_inference = config.task_for_inference
+            self.tasks = config.tasks
+            self.tasks_sample_ratio = config.get("tasks_sample_ratio", None)
+
+    @classmethod
+    def config_path(cls):
+        return "configs/models/fashionvil/defaults.yaml"
+
+    def train(self, mode=True):
+        super().train(mode)
+        if self.freeze_image_encoder:
+            self.image_encoder.eval()
+
+    def build(self):
+        self.image_encoder = build_image_encoder(
+            self.config.image_encoder, self.config.direct_features_input
+        )
+        if self.freeze_image_encoder:
+            self.image_encoder = self.image_encoder.eval()
+            for param in self.image_encoder.parameters():
+                param.requires_grad = False
+        if self.training_head_type == "pretraining":
+            self.model = FashionViLForPretraining(self.config)
+        elif self.training_head_type == "classification":
+            self.model = FashionViLForClassification(self.config)
+        elif self.training_head_type == "composition":
+            self.model = FashionViLForComposition(self.config)
+        elif self.training_head_type == "contrastive":
+            self.model = FashionViLForContrastive(self.config)
+        else:
+            raise NotImplementedError
+
+        if self.config.special_visual_initialize:
+            self.model.bert.embeddings.initialize_visual_from_pretrained()
+
+        if getattr(self.config, "freeze_base", False):
+            for p in self.model.bert.parameters():
+                p.requires_grad = False
+
+    def get_optimizer_parameters(self, config):
+        base_lr = config.optimizer.params.lr
+        weight_decay = config.optimizer.params.weight_decay
+        lr_multiplier = self.config.lr_multiplier
+
+        image_encoder_params = [
+            {
+                "params": filter_grads(self.image_encoder.parameters()),
+                "lr": base_lr * lr_multiplier,
+            }
+        ]
+        # lr_filter = []
+        # lr_filter.append("bert.embeddings.projection.weight")
+        # lr_filter.append("bert.embeddings.projection.bias")
+        # if self.training_head_type == "classification":
+        #     lr_filter.append("classifier")
+        # elif self.training_head_type == "pretraining":
+        #     lr_filter.append("heads")
+        # bert_params = get_fashionvil_configured_parameters(
+        #     self.model,
+        #     base_lr,
+        #     weight_decay,
+        #     lr_filter,
+        #     lr_multiplier,
+        # )
+        # return image_encoder_params + bert_params
+        return image_encoder_params
+
+    def forward(self, sample_list: Dict[str, Tensor]) -> Dict[str, Tensor]:
+        # print(sample_list.image.shape, sample_list.input_ids.shape, sample_list.segment_ids.shape, sample_list.input_mask.shape, sample_list.targets.shape)
+        # # torch.Size([64, 3, 224, 224]) torch.Size([64, 75]) torch.Size([64, 75]) torch.Size([64, 75]) torch.Size([64])
+        if self.training_head_type == "pretraining":
+            if self.training:
+                random_idx = choice(len(self.tasks), p=self.tasks_sample_ratio)
+                random_idx = broadcast_tensor(torch.tensor(random_idx).cuda())
+                sample_list.task = self.tasks[random_idx.item()]
+            else:
+                sample_list.task = self.task_for_inference
+
+        if self.training_head_type == "composition":
+            sample_list.ref_image = self.image_encoder(sample_list.ref_image)
+            sample_list.tar_image = self.image_encoder(sample_list.tar_image)
+        # elif self.double_view and self.training:
+        #     sample_list.original_image = torch.cat(
+        #         (sample_list.image, sample_list.dv_image), dim=0
+        #     )
+        #     sample_list.image_0 = self.image_encoder(sample_list.image)
+        #     sample_list.image_1 = self.image_encoder(sample_list.dv_image)
+        #     sample_list.image = torch.cat(
+        #         (sample_list.image_0, sample_list.image_1), dim=0
+        #     )
+        else:
+            if sample_list.image.dim() > 4:
+                sample_list.image = torch.flatten(sample_list.image, end_dim=-4)
+                sample_list.image_id = torch.flatten(sample_list.image_id, end_dim=-2)
+            if sample_list.input_mask.dim() > 2:
+                sample_list.input_mask = torch.flatten(
+                    sample_list.input_mask, end_dim=-2
+                )
+                sample_list.input_ids = torch.flatten(sample_list.input_ids, end_dim=-2)
+                sample_list.segment_ids = torch.flatten(
+                    sample_list.segment_ids, end_dim=-2
+                )
+            if sample_list.targets.dim() > 1:
+                sample_list.targets = torch.flatten(sample_list.targets)
+            sample_list.original_image = sample_list.image
+            sample_list.image = self.image_encoder(sample_list.image)
+
+        if self.training_head_type == "pretraining" and sample_list.task == "icc":
+            sample_list.dv_image = self.image_encoder(sample_list.dv_image)
+
+        return self.model(sample_list)
